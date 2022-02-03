5f8d4b03efa36f76583584511fa8976a0451e209
sumukshashidhar <sumukuuu@gmail.com>
2022-02-02 23:50:37 -0600

    feat[routes]: add server number

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 
 
     app.get("/", async function ( _ , res) {
-        res.render("index", {name: process.env.NAME, current_time: Date.now()})
+        res.render("index", {name: process.env.NAME, current_time: Date.now(), server_number: process.env.SERVER_NUMBER})
     })
 
     return app;
